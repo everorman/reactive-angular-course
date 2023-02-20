aae9e821c4370c9ae7b10eaf150b7bdc1811e646
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-02-20 13:06:07 -0500

    Implementing loading in course-dialog-component

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,17 +1,18 @@
-import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import { Course } from "../model/course";
-import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { AfterViewInit, Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import * as moment from 'moment';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
-import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
+import { Course } from "../model/course";
+import { CoursesService } from '../services/courses.service';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+    styleUrls: ['./course-dialog.component.css'],
+    providers: [
+        LoadingService
+    ]
 })
 export class CourseDialogComponent implements AfterViewInit {
 
@@ -43,7 +44,9 @@ export class CourseDialogComponent implements AfterViewInit {
 
     save() {
         const changes = this.form.value;
-        this.coursesService.saveCourse(this.course.id, changes)
+        const saveCouse$ = this.coursesService.saveCourse(this.course.id, changes);
+
+        this.loadingService.showLoaderUntilCompleted(saveCouse$)
             .subscribe(val => {
                 this.dialogRef.close(val)
             })
